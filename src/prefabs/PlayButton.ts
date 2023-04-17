ed797c823d616bd1620ac3a2a8f66f17ac8401b6
Richard Chung <iwannaplaygamesjp@gmail.com>
2023-04-17 20:14:38 +0000

    added camera follow and bounds
    created prototype first level
    
    fixed starter levels
    
    add timer bar
    
    Removed timebar.scene and timebar prefab
    
    set bounds for players
    
    Level 2. Floating plat has big collision shape

--- src/prefabs/PlayButton.ts
+++ src/prefabs/PlayButton.ts
@@ -24,7 +24,7 @@ export default class PlayButton extends Phaser.GameObjects.Image {
 		const startSceneActionScript = new StartSceneActionScript(onPointerDownScript);
 
 		// startSceneActionScript (prefab fields)
-		startSceneActionScript.sceneKey = "Level";
+		startSceneActionScript.sceneKey = "Level2";
 
 		/* START-USER-CTR-CODE */
 		// this.setInteractive();
--- src/scenes/Level2.ts
+++ src/scenes/Level2.ts
@@ -1,3 +1,4 @@
+
 // You can write more code here
 
 /* START OF COMPILED CODE */
@@ -8,13 +9,12 @@ import Player from "../prefabs/Player";
 import OnKeyBoardJustDownScript from "../prefabs/scriptNodes/OnKeyBoardJustDownScript";
 import ScriptNode from "../prefabs/scriptNodes/ScriptNode";
 /* START-USER-IMPORTS */
-import { ANIM_P_RUN, ANIM_P_DBL_JUMP } from "../animations";
 /* END-USER-IMPORTS */
 
-export default class Level extends Phaser.Scene {
+export default class Level2 extends Phaser.Scene {
 
 	constructor() {
-		super("Level");
+		super("Level2");
 
 		/* START-USER-CTR-CODE */
 		// Write your code here.
@@ -28,19 +28,19 @@ export default class Level extends Phaser.Scene {
 
 	editorCreate(): void {
 
-		// startLevel
-		const startLevel = this.add.tilemap("startLevel");
-		startLevel.addTilesetImage("Terrain (16x16)", "Terrain (16x16)");
-		startLevel.addTilesetImage("Clouds V2-2", "Clouds_V2-2");
+		// lv2
+		const lv2 = this.add.tilemap("lv2");
+		lv2.addTilesetImage("Terrain (16x16)", "Terrain (16x16)");
+		lv2.addTilesetImage("Clouds V2-2", "Clouds_V2-2");
 
 		// background_1
-		startLevel.createLayer("background", ["Clouds V2-2"], 0, 0);
+		lv2.createLayer("background", ["Clouds V2-2"], 0, 0);
 
 		// ground_1
-		const ground_1 = startLevel.createLayer("ground", ["Terrain (16x16)"], 0, 0);
+		const ground_1 = lv2.createLayer("ground", ["Terrain (16x16)"], 0, 0);
 
 		// player_1
-		const player_1 = new Player(this, 110, 148);
+		const player_1 = new Player(this, 110, 140);
 		this.add.existing(player_1);
 		player_1.scaleX = 1;
 		player_1.scaleY = 1;
@@ -64,7 +64,7 @@ export default class Level extends Phaser.Scene {
 		this.onKeyBoardJustDownScript = onKeyBoardJustDownScript;
 		this.debugScript = debugScript;
 		this.collider = collider;
-		this.startLevel = startLevel;
+		this.lv2 = lv2;
 
 		this.events.emit("scene-awake");
 	}
@@ -73,14 +73,15 @@ export default class Level extends Phaser.Scene {
 	private onKeyBoardJustDownScript!: OnKeyBoardJustDownScript;
 	private debugScript!: ScriptNode;
 	private collider!: Phaser.Physics.Arcade.Collider;
-	private startLevel!: Phaser.Tilemaps.Tilemap;
+	private lv2!: Phaser.Tilemaps.Tilemap;
 
 	/* START-USER-CODE */
+
+	// Write your code here
 	private WKey?: Phaser.Input.Keyboard.Key
 	private cursors?: Phaser.Types.Input.Keyboard.CursorKeys
 	private theme?: Phaser.Sound.BaseSound
 
-	// Write your code here
 	create() {
 
 		this.editorCreate();
@@ -101,50 +102,7 @@ export default class Level extends Phaser.Scene {
 			}
 
 		}
-		// Create and store keyboard input
-		// this.WKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
 	}
-
-	update() {
-
-		// if (this.WKey && this.theme && Phaser.Input.Keyboard.JustDown(this.WKey)) {
-		// 	console.log('W Key is just pressed')
-		// 	if(!this.theme.isPlaying)
-		// 	{
-		// 		this.theme.play()
-		// 	}
-		// 	else if (this.theme.isPlaying)
-		// 	{
-		// 		this.theme.pause()
-		// 	}
-		// }
-
-    	// if (this.cursors.left.isDown) {
-        // 	this.character.setVelocityX(-200);
-        // 	this.character.anims.play("run", true);
-        // 	this.character.flipX = true;
-    	// } else if (this.cursors.right.isDown) {
-		// 	this.character.setVelocityX(200);
-		// 	this.character.anims.play("run", true);
-		// 	this.character.flipX = false;
-		// } else {
-		// 	this.character.setVelocityX(0);
-		// 	this.character.anims.play("idle", true);
-		// }
-
-		// // Jumping
-		// if (Phaser.Input.Keyboard.JustDown(this.cursors.up) && this.character.body.onFloor()) {
-		// 	this.character.setVelocityY(-350);
-		// }
-
-		// // Jump and fall animations
-		// if (this.character.body.velocity.y < 0) {
-		// 	this.character.anims.play("jump", true);
-		// } else if (this.character.body.velocity.y > 0 && !this.character.body.touching.down) {
-		// 	this.character.anims.play("fall", true);
-		// }
-	}
-
 
 	/* END-USER-CODE */
 }
