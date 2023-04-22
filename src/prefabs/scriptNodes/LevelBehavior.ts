8c2449308f2627517bd260b31a8f789b619288fa
Richard <iwannaplaygamesjp@gmail.com>
2023-04-21 23:40:17 -0400

    reorganising level behavior

--- src/prefabs/scriptNodes/LevelBehavior.ts
+++ src/prefabs/scriptNodes/LevelBehavior.ts
@@ -5,6 +5,8 @@
 
 import ScriptNode from "./ScriptNode";
 import Phaser from "phaser";
+import CameraBounds from "./CameraBounds";
+import WorldBounds from "./WorldBounds";
 /* START-USER-IMPORTS */
 import Player from "../Player";
 import PlayerContainer from "../PlayerContainer";
@@ -18,26 +20,77 @@ export default class LevelBehavior extends ScriptNode {
 	constructor(parent: ScriptNode | Phaser.GameObjects.GameObject | Phaser.Scene) {
 		super(parent);
 
+		// cameraBounds
+		const cameraBounds = new CameraBounds(this);
+
+		// worldBounds
+		const worldBounds = new WorldBounds(this);
+
+		this.cameraBounds = cameraBounds;
+		this.worldBounds = worldBounds;
+
 		/* START-USER-CTR-CODE */
 		// Write your code here.
 		/* END-USER-CTR-CODE */
 	}
 
+	private cameraBounds: CameraBounds;
+	private worldBounds: WorldBounds;
 	public player!: Player;
-	public groundLayer!: any;
+	public groundLayer!: Phaser.Tilemaps.TilemapLayer;
 	public hydrantList!: FireHydrant[];
 	public goal!: Goal;
 	public newspaper!: Newspaper;
 
 	/* START-USER-CODE */
+	private cursors?: Phaser.Types.Input.Keyboard.CursorKeys
+	
+	awake() {
+		const {scene} = this
+
+		//  passing the total width and height from the level scene to create the camera bounds
+		scene.events.once("setup-cam-bounds", this.setupCameraBounds, this)
+		scene.events.once("setup-world-bounds", this.setupWorldBounds, this)
+	}
 
 	// Write your code here.
 	start() {
 		const {scene} = this
 
+		
+
+		// setting up all the colliders here
 		scene.physics.add.collider(this.newspaper, this.groundLayer)
 		scene.physics.add.collider(this.hydrantList, this.groundLayer)
-		
+		scene.physics.add.collider(this.player, this.groundLayer)
+		//@ts-ignore
+		scene.physics.add.collider(this.player, this.newspaper, this.handlePlayerNewsPaper)
+		//@ts-ignore
+		scene.physics.add.collider(this.player, this.hydrantList, this.handlePlayerHydrant)
+		scene.physics.add.collider(this.player, this.goal)
+
+		// setting all the player inputs here and fire events to the player instead
+
+	}
+
+	setupCameraBounds(_boundW: number, _boundH: number) {
+		this.cameraBounds.boundWidth = _boundW;
+		this.cameraBounds.boundHeight = _boundH
+	}
+
+	setupWorldBounds(_boundW: number, _boundH: number) {
+		this.worldBounds.width = _boundW;
+		this.worldBounds.height = _boundH;
+	}
+
+	handlePlayerNewsPaper(player: Player, newspaper: Newspaper) {
+		newspaper.disableBody(true, true)
+		player.equip(1)
+	}
+
+	handlePlayerHydrant(player: Player, hydrant: FireHydrant) {
+		player.pee()
+		hydrant.disableBody()
 	}
 
 	/* END-USER-CODE */
--- src/scenes/Level.ts
+++ src/scenes/Level.ts
@@ -136,11 +136,14 @@ export default class Level extends Phaser.Scene {
 
 		this.editorCreate();
 
-		this.cameraBounds.boundWidth = this.lv1.width * this.lv1.tileWidth
-		this.cameraBounds.boundHeight = this.lv1.height * this.lv1.tileHeight
+		this.events.emit("setup-cam-bounds", this.lv1.width * this.lv1.tileWidth, this.lv1.height * this.lv1.tileHeight)
+		this.events.emit("setup-world-bounds", this.lv1.width * this.lv1.tileWidth, this.lv1.height * this.lv1.tileHeight)
 
-		this.physics.add.collider(this.newspaper, this.ground_1)
-		this.physics.add.collider(this.hydrantList, this.ground_1)
+		// this.cameraBounds.boundWidth = this.lv1.width * this.lv1.tileWidth
+		// this.cameraBounds.boundHeight = this.lv1.height * this.lv1.tileHeight
+
+		// this.physics.add.collider(this.newspaper, this.ground_1)
+		// this.physics.add.collider(this.hydrantList, this.ground_1)
 
 		//@ts-ignore
 		const p_news = this.physics.add.collider(this.player_1, this.newspaper, this.handlePlayerNewsPaper)
