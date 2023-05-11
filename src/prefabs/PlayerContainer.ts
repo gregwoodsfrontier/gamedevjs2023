e4ebbe67dd99e6bd6153abddd150e98e65e18fdd
Richard Chung <iwannaplaygamesjp@gmail.com>
2023-05-11 21:52:35 +0000

    added detection box

--- src/prefabs/PlayerContainer.ts
+++ src/prefabs/PlayerContainer.ts
@@ -18,16 +18,26 @@ export default class PlayerContainer extends Phaser.GameObjects.Container {
 		const player_1 = new Player(scene, 0, 0);
 		this.add(player_1);
 
+		// detectionBox
+		const detectionBox = scene.add.rectangle(-4, -6, 32, 8);
+		detectionBox.isFilled = true;
+		detectionBox.fillAlpha = 0.3;
+		this.add(detectionBox);
+
 		// playerController
 		const playerController = new PlayerController(this);
 
 		// playerController (prefab fields)
 		playerController.sprite = player_1;
 
+		this.detectionBox = detectionBox;
+
 		/* START-USER-CTR-CODE */
 		// Write your code here.
 		/* END-USER-CTR-CODE */
 	}
+
+	public detectionBox: Phaser.GameObjects.Rectangle;
 
 	/* START-USER-CODE */
 
