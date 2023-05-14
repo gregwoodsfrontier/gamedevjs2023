75d9fbed416a517a79fb5c1f4b35dbfbaa2bb223
Richard <iwannaplaygamesjp@gmail.com>
2023-05-14 16:59:39 -0400

    added tunnel zone prefabs

---
+++ src/prefabs/TunnelZone.ts
@@ -0,0 +1,35 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import Phaser from "phaser";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class TunnelZone extends Phaser.GameObjects.Rectangle {
+
+	constructor(scene: Phaser.Scene, x?: number, y?: number, width?: number, height?: number) {
+		super(scene, x ?? 0, y ?? 0, width ?? 16, height ?? 16);
+
+		this.scaleX = 2.000339879116426;
+		this.setOrigin(0, 0);
+		this.isFilled = true;
+		this.fillColor = 14868512;
+		this.fillAlpha = 0.6;
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
